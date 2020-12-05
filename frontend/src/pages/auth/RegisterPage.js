f57f7f71e5164c3ee57fa9c767bb256f673ef4ee
abdigit <abdulkadirahmed1985@gmail.com>
2020-12-05 01:56:10 +0100

    user register page form

--- frontend/src/pages/auth/RegisterPage.js
+++ frontend/src/pages/auth/RegisterPage.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const RegisterPage = () => {
-  return <div>Regidst</div>;
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = () => {
+    //
+  };
+
+  return (
+    <div className="container p-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h4>Register</h4>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="email"
+              className="form-control"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              autoFocus
+            />
+
+            <button type="submit" className="btn btn-raised btn-block mt-3">
+              Register
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;

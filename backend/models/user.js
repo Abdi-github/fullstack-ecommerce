8e026298911174df98ec88b897906193cbb98e08
abdigit <abdulkadirahmed1985@gmail.com>
2020-12-08 00:21:12 +0100

    add User schema

---
+++ backend/models/user.js
@@ -0,0 +1,24 @@
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    role: {
+      type: String,
+      default: "subscriber",
+    },
+    cart: {
+      type: Array,
+      default: [],
+    },
+    address: String,
+    //   wishlist: [{ type: ObjectId, ref: "Product" }],
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;

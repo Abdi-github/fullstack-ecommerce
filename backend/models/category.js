86133114c61ad8bc70b8c13e6c8a2c7311d37a82
abdigit <abdulkadirahmed1985@gmail.com>
2020-12-09 14:00:36 +0100

    category schema created

---
+++ backend/models/category.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const categorySchema = new mongoose.Schema(
+  {
+   
+    name: {
+      type: String,
+      required: true,
+      index: true,
+      trim:true,
+      minlength:[3, "Too short characters"],
+      maxlength:[32, "Too long characters"],
+    },
+    slug: {
+      type: String,
+      unique: true,
+      lowercase:true,
+      index:true,
+    },
+    
+  { timestamps: true }
+);
+
+const Category = mongoose.model("Category", categorySchema);
+export default Category;

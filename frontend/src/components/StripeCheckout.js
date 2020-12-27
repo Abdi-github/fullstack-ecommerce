832fbf417d5b43c711b733a1f300cba8ee11bb0f
abdigit <abdulkadirahmed1985@gmail.com>
2020-12-27 13:23:12 +0100

    payment with stripe implemented

--- frontend/src/components/StripeCheckout.js
+++ frontend/src/components/StripeCheckout.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import { createPaymentIntent } from "../functions/stripe";
 
@@ -23,12 +24,38 @@ const StripeCheckout = ({ history }) => {
     });
   }, []);
 
-  const handleSubmit = async (e) => {
-    //
+  const handleChange = async (e) => {
+    // listen for changes in the card element
+    // and display any errors as the custoemr types their card details
+    setDisabled(e.empty); // disable pay button if errors
+    setError(e.error ? e.error.message : ""); // show error message
   };
 
-  const handleChange = async (e) => {
-    //
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setProcessing(true);
+
+    const payload = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: e.target.name.value,
+        },
+      },
+    });
+
+    if (payload.error) {
+      setError(`Payment failed ${payload.error.message}`);
+      setProcessing(false);
+    } else {
+      // here you get result after successful payment
+      // create order and save in database for admin to process
+      // empty user cart from redux store and local storage
+      console.log(JSON.stringify(payload, null, 4));
+      setError(null);
+      setProcessing(false);
+      setSuccess(true);
+    }
   };
 
   const cartStyle = {
@@ -51,6 +78,11 @@ const StripeCheckout = ({ history }) => {
 
   return (
     <>
+      <p className={success ? "result-message" : "result-message hidden"}>
+        Payment Successful.{" "}
+        <Link to="/user/history">See it in your purchase history.</Link>
+      </p>
+
       <form id="payment-form" className="stripe-form" onSubmit={handleSubmit}>
         <CardElement
           id="card-element"
@@ -65,6 +97,12 @@ const StripeCheckout = ({ history }) => {
             {processing ? <div className="spinner" id="spinner"></div> : "Pay"}
           </span>
         </button>
+        <br />
+        {error && (
+          <div className="card-error text-danger" role="alert">
+            {error}
+          </div>
+        )}
       </form>
     </>
   );
